6ab8f276689fd3359ce874b44d05b10598b9069d
benser1 <briganenser28@gmail.com>
2015-11-09 12:08:21 -0700

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
-# Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit - 1
+  end
+
+#creates array << multiplese of 3&5
+  def collect_multiples
+    (1..@limit).select{|num| num%3 == 0 || num%5 == 0}
+  end
+
+  #finds numbers divisable by 3&5 > array
+  def sum_multiples
+    @sum35 = (1..@limit).select{|num| num%3 == 0 || num%5 == 0}
+    @sum35 = @sum35.reduce(:+)
+    @sum35
+  end
+end
